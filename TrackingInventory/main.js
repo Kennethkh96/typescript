f23bd5f15c56ca16f7bea6bd9e3cc24ab32df18f
Kenneth <kennethkh96@gmail.com>
2017-09-13 12:42:34 +0200

    Added solution

--- TrackingInventory/main.js
+++ TrackingInventory/main.js
@@ -7,17 +7,88 @@ var Inventory = /** @class */ (function () {
         var name = document.getElementById("name");
         var serial = document.getElementById("serial");
         var value = document.getElementById("value");
+        if (!this.Validate(name, serial, value))
+            return;
+        var invitem = new InventoryItem(name.value, serial.value, +value.value);
+        this.inv.push(invitem);
+        this.PrintInventory();
+        name.value = "";
+        serial.value = "";
+        value.value = "";
+    };
+    Inventory.prototype.PrintBySearch = function () {
+        var elem = document.getElementById("searchText");
+        var search = elem.value;
+        document.getElementById("printingTable").innerHTML = "";
+        var table = document.getElementById("printingTable");
+        table.appendChild(this.CreateTableHeader());
+        for (var index = 0; index < this.inv.length; index++) {
+            var el = this.inv[index];
+            if (el.getName() == search || el.getSerial() == search || el.getValue() == +search) {
+                table.appendChild(this.CreateTableRow(el));
+            }
+        }
+    };
+    Inventory.prototype.PrintInventory = function () {
+        document.getElementById("printingTable").innerHTML = "";
+        var table = document.getElementById("printingTable");
+        table.appendChild(this.CreateTableHeader());
+        for (var index = 0; index < this.inv.length; index++) {
+            var element = this.inv[index];
+            table.appendChild(this.CreateTableRow(element));
+        }
+    };
+    Inventory.prototype.CreateTableHeader = function () {
+        var row = document.createElement("tr");
+        row.appendChild(this.CreateHtmlElem("th", "Name"));
+        row.appendChild(this.CreateHtmlElem("th", "Serial"));
+        row.appendChild(this.CreateHtmlElem("th", "Value"));
+        return row;
+    };
+    Inventory.prototype.CreateTableRow = function (item) {
+        var row = document.createElement("tr");
+        row.appendChild(this.CreateHtmlElem("td", item.getName()));
+        row.appendChild(this.CreateHtmlElem("td", item.getSerial()));
+        row.appendChild(this.CreateHtmlElem("td", "$" + item.getValue()));
+        return row;
+    };
+    Inventory.prototype.CreateHtmlElem = function (type, text) {
+        var elem = document.createElement(type);
+        var textnode = document.createTextNode(text);
+        elem.appendChild(textnode);
+        return elem;
     };
     Inventory.prototype.Validate = function (name, serial, value) {
-        if (name.value == null)
+        /*console.log("validate");
+        console.log("name: " + name.value);
+        console.log("serial " + serial.value);
+        console.log("value: " + value.value);
+        */
+        if (name.value == "")
             return false;
-        if (serial.value == null)
+        if (serial.value == "")
             return false;
-        if (value.value == null && isNaN(+value.value))
+        if (value.value == "" || isNaN(+value.value))
             return false;
-        alert("yay");
         return true;
     };
     return Inventory;
 }());
-var inv = new Inventory();
+var InventoryItem = /** @class */ (function () {
+    function InventoryItem(name, serial, value) {
+        this.name = name;
+        this.serial = serial;
+        this.value = value;
+    }
+    InventoryItem.prototype.getName = function () {
+        return this.name;
+    };
+    InventoryItem.prototype.getSerial = function () {
+        return this.serial;
+    };
+    InventoryItem.prototype.getValue = function () {
+        return this.value;
+    };
+    return InventoryItem;
+}());
+var inven = new Inventory();
--- TrackingInventory/main.ts
+++ TrackingInventory/main.ts
@@ -1,27 +1,131 @@
 class Inventory
 {
-    private inv: JSON[] = [];
+    private inv: InventoryItem[] = [];
 
     Add(): void
     {
         let name = <HTMLInputElement> document.getElementById("name");
         let serial = <HTMLInputElement> document.getElementById("serial");
         let value = <HTMLInputElement> document.getElementById("value");
+
+        if (! this.Validate(name, serial, value))
+            return;
+
+        let invitem = new InventoryItem(name.value, serial.value, +value.value);
+
+        this.inv.push(invitem);
+        this.PrintInventory();
+
+        name.value = "";
+        serial.value = "";
+        value.value = "";
+    }
+
+    PrintBySearch()
+    {
+        let elem = <HTMLInputElement> document.getElementById("searchText");
+        let search = elem.value;
+        document.getElementById("printingTable").innerHTML = "";
+        let table = <HTMLTableElement> document.getElementById("printingTable");
+        table.appendChild(this.CreateTableHeader());
+        for (let index = 0; index < this.inv.length; index++) {
+            let el = this.inv[index];
+            
+            if (el.getName() == search || el.getSerial() == search || el.getValue() == +search)
+            {
+                table.appendChild(this.CreateTableRow(el));
+            }
+        }
+    }
+
+    PrintInventory()
+    {
+        document.getElementById("printingTable").innerHTML = "";
+        let table = <HTMLTableElement> document.getElementById("printingTable");
+        table.appendChild(this.CreateTableHeader());
+        for (let index = 0; index < this.inv.length; index++) {
+            let element = this.inv[index];
+            table.appendChild(this.CreateTableRow(element));
+        }
+    }
+
+    CreateTableHeader()
+    {
+        let row = document.createElement("tr");
+        row.appendChild(this.CreateHtmlElem("th", "Name"));
+        row.appendChild(this.CreateHtmlElem("th", "Serial"));
+        row.appendChild(this.CreateHtmlElem("th", "Value"));
+
+        return row;
+    }
+
+    CreateTableRow(item: InventoryItem): HTMLTableRowElement
+    {
+        let row = document.createElement("tr");
+        row.appendChild(this.CreateHtmlElem("td", item.getName()));
+        row.appendChild(this.CreateHtmlElem("td", item.getSerial()));
+        row.appendChild(this.CreateHtmlElem("td", "$" + item.getValue()));
+
+        return row;
+    }
+
+    CreateHtmlElem(type: string, text: string): HTMLElement
+    {
+        let elem = document.createElement(type);
+        let textnode = document.createTextNode(text);
+        elem.appendChild(textnode);
+
+        return elem;
     }
 
 
     Validate(name: HTMLInputElement, serial: HTMLInputElement, value: HTMLInputElement): boolean
     {
-        if (name.value == null)
-            return false;
-        if (serial.value == null)
-            return false;
-        if (value.value == null && isNaN(+value.value))
-            return false;
 
-        alert("yay");
+        /*console.log("validate");
+        console.log("name: " + name.value);
+        console.log("serial " + serial.value);
+        console.log("value: " + value.value);
+        */
+        if (name.value == "")
+            return false;
+        if (serial.value == "")
+            return false;
+        if (value.value == "" || isNaN(+value.value))
+            return false;
+        
         return true;
     }
 }
 
-let inv = new Inventory();
+class InventoryItem
+{
+    private name: string;
+    private serial: string;
+    private value: number;
+
+    constructor(name: string, serial: string, value: number)
+    {
+        this.name = name;
+        this.serial = serial;
+        this.value = value;
+    }
+
+    public getName()
+    {
+        return this.name;
+    }
+
+    public getSerial()
+    {
+        return this.serial;
+    }
+
+    public getValue()
+    {
+        return this.value;
+    }
+}
+
+let inven = new Inventory();
+
